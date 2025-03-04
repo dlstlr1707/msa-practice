f67a97e2806ae7d78ac850bb69aea3f89a427fe6
dlstlr1707 <dlstlr1707@naver.com>
2025-03-04 10:40:29 +0900

    25/03/04 first generated

---
+++ practiceNum2/src/main/java/com/example/practicenum2/PracticeNum2Application.java
@@ -0,0 +1,13 @@
+package com.example.practicenum2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeNum2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeNum2Application.class, args);
+    }
+
+}
---
+++ practiceNum2/src/test/java/com/example/practicenum2/PracticeNum2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practicenum2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeNum2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
